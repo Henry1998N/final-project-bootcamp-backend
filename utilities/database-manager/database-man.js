0044e512455c256d0f8ce8ea7c39e15e7006d50f
Henry1998N <henry.nur137@gmail.com>
2023-04-13 12:17:28 +0300

    added env file

--- utilities/database-manager/database-man.js
+++ utilities/database-manager/database-man.js
@@ -1,6 +1,14 @@
 const mongoose = require("mongoose");
+require("dotenv").config(); // Load the .env file
 const DataBaseManager = function () {
-  const connect = function () {};
+  const connect = function () {
+    mongoose
+      .connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+      })
+      .catch((err) => console.log(err));
+    //
+  };
   return {
     connect,
   };

50fc9ed5fd5137578dcb6679862a3e955e80546a
BadeaFreh <71319309+BadeaFreh@users.noreply.github.com>
2023-04-15 17:40:47 +0300

    added mock data for residents and apartments

---
+++ mock-data/apartments.js
@@ -0,0 +1,75 @@
+const apartments = [
+    {
+        apartmentName: 'Peaceful Retreat',
+        address: {
+            value: '17 Ben Yehuda Street, Tel Aviv',
+            wazeLink: 'https://www.waze.com/ul/hsv8zjyw',
+        },
+        budget: 6000,
+        residents: [
+            {
+                residentId: 'RES001',
+            },
+            {
+                residentId: 'RES002',
+            },
+            {
+                residentId: 'RES003',
+            },
+        ],
+    },
+    {
+        apartmentName: 'Cozy Haven',
+        address: {
+            value: '10 Herzl Street, Haifa',
+            wazeLink: 'https://www.waze.com/ul/hsv8zjyw',
+        },
+        budget: 4500,
+        residents: [
+            {
+                residentId: 'RES004',
+            },
+            {
+                residentId: 'RES005',
+            },
+        ],
+    },
+    {
+        apartmentName: 'Simple Living',
+        address: {
+            value: '8 Rothschild Boulevard, Tel Aviv',
+            wazeLink: 'https://www.waze.com/ul/hsv8zjyw',
+        },
+        budget: 7000,
+        residents: [
+            {
+                residentId: 'RES006',
+            },
+            {
+                residentId: 'RES007',
+            },
+            {
+                residentId: 'RES008',
+            },
+            {
+                residentId: 'RES009',
+            },
+        ],
+    },
+    {
+        apartmentName: 'Comfortable Home',
+        address: {
+            value: '12 Dizengoff Street, Tel Aviv',
+            wazeLink: 'https://www.waze.com/ul/hsv8zjyw',
+        },
+        budget: 9000,
+        residents: [
+            {
+                residentId: 'RES010',
+            },
+            {
+                residentId: 'RES011',
+            },
+        ],
+    },
+]

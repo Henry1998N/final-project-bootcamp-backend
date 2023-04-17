9a91f5f4f3eef4903c78a83ee58aace4aeba73d9
Henry1998N <henry.nur137@gmail.com>
2023-04-17 17:20:04 +0300

    added resident query to update

--- utilities/routes-functions.js/resident-querys.js
+++ utilities/routes-functions.js/resident-querys.js
@@ -33,4 +33,39 @@ const getResidentDetailsByQueryString = function (
     }
   );
 };
-module.exports = { generateResident, getResidentDetailsByQueryString };
+const updateMedicineStatus = function (resident, period, medicationName) {
+  const residentCopy = resident[0];
+
+  const medication = residentCopy.medication;
+  const periodMedication = medication[period];
+  const medicineIndex = periodMedication.findIndex(
+    (m) => m.medicationName == medicationName
+  );
+  periodMedication[medicineIndex].status = true;
+  return residentCopy;
+};
+
+const updateResidentMedication = async function (
+  residentId,
+  period,
+  medicationName
+) {
+  const resident = await getResidentDetailsByQueryString([], residentId);
+  const residentCopy = updateMedicineStatus(resident, period, medicationName);
+  return Resident.findOneAndUpdate(
+    { residentId: residentCopy.residentId },
+    { medication: residentCopy.medication },
+    { new: true }
+  )
+    .then((doc) => {
+      return doc;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+module.exports = {
+  generateResident,
+  getResidentDetailsByQueryString,
+  updateResidentMedication,
+};

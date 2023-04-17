c6309ae19c4e30bccf3eb1f4ff6f622a937d51ff
Henry1998N <henry.nur137@gmail.com>
2023-04-17 10:22:37 +0300

    fixed get

--- server-manager/routes/resident-api.js
+++ server-manager/routes/resident-api.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const residentHandler = require("../../utilities/controller/resident-handler");
-router.get("/resident/:id", residentHandler.getResidentDetailsByQueryString);
+router.get("/:id", residentHandler.getResidentDetailsByQueryString);
 // router.post("/residents", function (req, res) {
 //   const resident = req.body.resident;
 //   residentsQuery.generateResident(resident);

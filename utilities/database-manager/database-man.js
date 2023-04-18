a04464b5973cbb5969439d8e6b0ce110efc582e3
Henry1998N <henry.nur137@gmail.com>
2023-04-18 11:24:46 +0300

    fixed database manager

--- utilities/database-manager/database-man.js
+++ utilities/database-manager/database-man.js
@@ -2,13 +2,10 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 const connectToDatabase = async () => {
   try {
-    await mongoose.connect(
-      "mongodb+srv://abdallahabukhdair:iliAar82pVvakMF6@cluster0.wvjvh0e.mongodb.net/HousingSystem?retryWrites=true&w=majority",
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }
-    );
+    await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("Connected to MongoDB");
   } catch (err) {
     console.error("Error connecting to MongoDB", err);

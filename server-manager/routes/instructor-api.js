f0f5b0e2c5ba94591ddada76abc3fb458d971f60
BadeaFreh <71319309+BadeaFreh@users.noreply.github.com>
2023-04-28 01:44:59 +0300

    route that gets all residents by their instructorId

--- server-manager/routes/instructor-api.js
+++ server-manager/routes/instructor-api.js
@@ -1,28 +1,34 @@
-const express = require("express");
-const router = express.Router();
-const {
-  signIn,
-  addNewInstructor,
-  updateInstructorApartments,
-  updateInstructor,
-  deleteInstructor,
-  addShift,
-  getInstructorShiftsById,
-} = require("../../utilities/controller/instructor-handler");
-const {
-  googleLogin,
-} = require("../../utilities/jwt-google-auth/google-Handler");
-const {
-  authenticateUser,
-} = require("../../utilities/middleware/middlewareFunctions");
-// router.use(authenticateUser);
-router.post("/sign-in", signIn);
-router.post("/auth/google-login", googleLogin);
-router.post("/instructors", addNewInstructor);
-router.post("/instructorApartment", updateInstructorApartments);
-router.put("/:instructorId", updateInstructor);
-router.delete("/:instructorId", deleteInstructor);
-router.post("/shifts/:instructorId", addShift);
-router.get("/shifts/:id", getInstructorShiftsById);
+const express = require('express')
+const router = express.Router()
 
-module.exports = router;
+const {
+    signIn,
+    addNewInstructor,
+    updateInstructorApartments,
+    updateInstructor,
+    deleteInstructor,
+    addShift,
+    getInstructorShiftsById,
+    getResidentsByInstructorId,
+} = require('../../utilities/controller/instructor-handler')
+
+const {
+    googleLogin,
+} = require('../../utilities/jwt-google-auth/google-Handler')
+
+const {
+    authenticateUser,
+} = require('../../utilities/middleware/middlewareFunctions')
+
+// router.use(authenticateUser);
+router.post('/sign-in', signIn)
+router.post('/auth/google-login', googleLogin)
+router.post('/instructors', addNewInstructor)
+router.post('/instructorApartment', updateInstructorApartments)
+router.put('/:instructorId', updateInstructor)
+router.delete('/:instructorId', deleteInstructor)
+router.post('/shifts/:instructorId', addShift)
+router.get('/residents/:id', getResidentsByInstructorId)
+router.get('/shifts/:id', getInstructorShiftsById)
+
+module.exports = router
--- utilities/routes-functions.js/instructor-queries.js
+++ utilities/routes-functions.js/instructor-queries.js
@@ -1,91 +1,101 @@
-require("dotenv").config();
-const Instructor = require("../../server-manager/models/instructor");
-const bcrypt = require("bcrypt");
-const { generateUser } = require("../routes-functions.js/user-queries");
-
-const salt = bcrypt.genSaltSync(parseInt(process.env.SALT_ROUNDS));
+require('dotenv').config()
+const Instructor = require('../../server-manager/models/instructor')
+const bcrypt = require('bcrypt')
+const { generateUser } = require('../routes-functions.js/user-queries')
+const salt = bcrypt.genSaltSync(parseInt(process.env.SALT_ROUNDS))
 
 const generateInstructor = function (instructor) {
-  const newInstructor = new Instructor({
-    instructorId: instructor.instructorId,
-    name: instructor.name,
-    apartments: [],
-    signature: instructor.signature,
-    email: instructor.email,
-    phoneNumber: instructor.phoneNumber,
-    password: bcrypt.hashSync(instructor.password, salt),
-    reports: [],
-    image: instructor.image,
-  });
-  return newInstructor;
-};
+    const newInstructor = new Instructor({
+        instructorId: instructor.instructorId,
+        name: instructor.name,
+        apartments: [],
+        signature: instructor.signature,
+        email: instructor.email,
+        phoneNumber: instructor.phoneNumber,
+        password: bcrypt.hashSync(instructor.password, salt),
+        reports: [],
+        image: instructor.image,
+    })
+    return newInstructor
+}
 
 const addInstructor = async function (instructor) {
-  const instructors = await getInstructor(instructor.email);
-  if (instructors.length > 0) {
-    return null;
-  }
-  const newInstructor = generateInstructor(instructor);
-  newInstructor.save();
-  generateUser(
-    {
-      id: newInstructor.instructorId,
-      email: newInstructor.email,
-      password: newInstructor.password,
-      objId: newInstructor._id,
-    },
-    "Instructor"
-  );
+    const instructors = await getInstructor(instructor.email)
+    if (instructors.length > 0) {
+        return null
+    }
+    const newInstructor = generateInstructor(instructor)
+    newInstructor.save()
+    generateUser(
+        {
+            id: newInstructor.instructorId,
+            email: newInstructor.email,
+            password: newInstructor.password,
+            objId: newInstructor._id,
+        },
+        'Instructor'
+    )
 
-  return newInstructor;
-};
+    return newInstructor
+}
 
 const getInstructor = async function (email) {
-  return Instructor.find({ email: email }).then((instructor) => {
-    return instructor;
-  });
-};
+    return Instructor.find({ email: email }).then((instructor) => {
+        return instructor
+    })
+}
 
 const updateInstructorApartmentsById = async function (
-  instructorId,
-  apartmentId
-) {
-  return await Instructor.findByIdAndUpdate(
     instructorId,
-    {
-      $push: { apartments: apartmentId },
-    },
-    { new: true }
-  );
-};
+    apartmentId
+) {
+    return await Instructor.findByIdAndUpdate(
+        instructorId,
+        {
+            $push: { apartments: apartmentId },
+        },
+        { new: true }
+    )
+}
 
 const updateInstructorById = async function (instructorId, name, phoneNumber) {
-  return await Instructor.findOneAndUpdate(
-    { instructorId: instructorId },
-    {
-      name: name,
-      phoneNumber: phoneNumber,
-    },
-    { new: true }
-  );
-};
+    return await Instructor.findOneAndUpdate(
+        { instructorId: instructorId },
+        {
+            name: name,
+            phoneNumber: phoneNumber,
+        },
+        { new: true }
+    )
+}
 
 const deleteInstructorById = async function (instructorId) {
-  return await Instructor.deleteOne({ intructorId: instructorId });
-};
+    return await Instructor.deleteOne({ intructorId: instructorId })
+}
+
 const getInstructorById = async function (instructorId) {
-  return Instructor.findById(instructorId);
-};
+    return Instructor.findById(instructorId)
+}
+
 const getInstructorShifts = async function (instructorId) {
-  const shifts = await Instructor.findById(instructorId).populate("shifts");
-  return shifts;
-};
+    const shifts = await Instructor.findById(instructorId).populate('shifts')
+    return shifts
+}
+
+const getInstructorApartments = async function (instructorId) {
+    const apartments = await Instructor.findOne({ instructorId }).populate(
+        'apartments'
+    )
+    return apartments
+}
+
 module.exports = {
-  getInstructor,
-  addInstructor,
-  updateInstructorApartmentsById,
-  updateInstructorById,
-  deleteInstructorById,
-  getInstructorById,
-  getInstructorShifts,
-};
+    getInstructor,
+    addInstructor,
+    updateInstructorApartmentsById,
+    updateInstructorById,
+    deleteInstructorById,
+    getInstructorById,
+    getInstructorShifts,
+    getInstructorApartments
+}
